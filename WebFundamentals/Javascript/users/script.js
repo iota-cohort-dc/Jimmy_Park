37aef65a31a660344b2eb8fd47032169b0e8225f
jimmymixer <jaepark75@gmail.com>
2017-03-01 18:51:47 -0500

    adding users.js

---
+++ WebFundamentals/Javascript/users/script.js
@@ -0,0 +1,25 @@
+
+
+//
+// function #####()
+// {
+//   $().click(function(){
+//     $()...('');
+//   });
+// }
+
+  $(document).ready(function(){
+
+    $('button').click(function(){
+      var fname = $('#f_name').val()
+      var lname = $('#l_name').val()
+      var email = $('#email').val()
+      var phone = $('#phone').val()
+      $('#table').append(
+        `<tr><td>${fname}</td><td>${lname}</td><td>${email}</td><td>${phone}</td>`
+
+
+      );
+    });
+
+});
