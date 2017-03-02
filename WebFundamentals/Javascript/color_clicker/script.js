5ac52fa724d47451993b3d58122bef307013ea7e
jimmymixer <jaepark75@gmail.com>
2017-03-01 20:06:32 -0500

    submit color_clicker

---
+++ WebFundamentals/Javascript/color_clicker/script.js
@@ -0,0 +1,40 @@
+
+
+function random_color()
+{
+   var rgb = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+   color = '#'  //this is what we'll return!
+   for(var i = 0; i < 6; i++)
+   {
+      x = Math.floor((Math.random()*16))
+      color += rgb[x];
+   }
+   return color;
+}
+
+$(document).ready(function(){
+
+  $('#large_box').click(function(){
+    //  alert('you clicked the big box!');
+    $(this).children().css('background',random_color);
+
+    // $(this).children().css('background', ***call function name or color****)
+    // children or siblings
+
+
+//comment this out when you have figured out what event.stopPropagation is used for
+  })
+  $('.side_box').click(function(event){
+     event.stopPropagation();
+     $(this).siblings('.middle_box').css('background-color',random_color);
+
+  })
+
+
+  $('.middle_box').click(function(event){
+     event.stopPropagation();
+    $(this).parent().css('background-color',random_color);
+
+  })
+
+});
