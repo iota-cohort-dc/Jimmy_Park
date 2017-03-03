3045806f256ee834493cedbaea4cfae7c63dbef6
jimmymixer <jaepark75@gmail.com>
2017-03-02 22:10:24 -0500

    Added pokemon

--- WebFundamentals/Javascript/users/script.js
+++ WebFundamentals/Javascript/users/script.js
@@ -15,6 +15,9 @@
       var lname = $('#l_name').val()
       var email = $('#email').val()
       var phone = $('#phone').val()
+
+      // $('#f_name').val('')
+
       $('#table').append(
         `<tr><td>${fname}</td><td>${lname}</td><td>${email}</td><td>${phone}</td>`
 
---
+++ WebFundamentals/Javascript/whats_that_pokemon/script.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function(){
+
+
+  for(var i = 1; i <= 151; i++){
+		$('#left').append('<img id=' + i + ' src="http://pokeapi.co/media/img/' + i + '.png">')
+	}
+
+  $(document).on('click', 'img', function(){
+    var id = $(this).attr('id');
+
+    $.get("http://pokeapi.co/api/v1/pokemon/"+id+"/", function(res){
+      $('#name').html(res.name)
+      var html_str = "";
+                         html_str += "<h4>Types</h4>";
+                         html_str += "<ul>";
+                         for(var i = 0; i < res.types.length; i++) {
+                             html_str += "<li>" + res.types[i].name + "</li>";
+                         }
+                         html_str += "</ul>";
+                         $("#types").html(html_str);
+                         $("#height").html(res.height);
+                         $("#weight").html(res.weight);
+                         $("#pic").html('<img id=' + id + ' src="http://pokeapi.co/media/img/' + id + '.png">');
+                     }, "json");
+    });
+}); //<------end..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
