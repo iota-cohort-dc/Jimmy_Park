dc2008832918cc9f8b849ae51c868cb84a27d90e
jimmymixer <jaepark75@gmail.com>
2017-03-07 08:05:40 -0500

    Sending Algorithm Practice,coin toss,fun with functions,hello world,multiples sum avg,names,scores and grades, stars,string and lists,try python

---
+++ Python/simple_searching/simple_searching.py
@@ -0,0 +1,10 @@
+# Simple Searching
+
+import re
+def get_matching_words(regex):
+ words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable", "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress", "millennia", "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid", "unbearable", "union", "videotape"]
+ matches = []
+ for word in words:
+ 	if re.search(regex,word):
+ 		matches.append(word)
+ return matches
